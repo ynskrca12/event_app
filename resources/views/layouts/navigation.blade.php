93771e496d9b31acd1de6becbb6ce582f09b0b1c
Yunus Kırca <ynskrca52@outlook.com>
2023-09-30 16:20:04 +0300

    Events and Galleries CRUD

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -14,6 +14,12 @@
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
+                    </x-nav-link>     
+                     <x-nav-link :href="route('events.index')" :active="request()->routeIs('events.index')">
+                        {{ __('Events') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('galleries.index')" :active="request()->routeIs('galleries.index')">
+                        {{ __('Gallery') }}
                     </x-nav-link>
                 </div>
             </div>
