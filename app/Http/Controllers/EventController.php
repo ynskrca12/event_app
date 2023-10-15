4148160ecd8c8151c025d9131dbbcb6cf9a4517e
Yunus Kırca <ynskrca52@outlook.com>
2023-10-15 22:56:50 +0300

    Display Events

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\CreateEventRequest;
 use App\Models\Country;
 use App\Models\Event;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 use Illuminate\Support\Facades\Storage;
@@ -18,7 +19,8 @@ class EventController extends Controller
      */
     public function index():View
     {
-        return view('events.index');
+        $events = Event::with('country')->get();
+        return view('events.index',compact('events'));
     }
 
     /**
@@ -27,8 +29,8 @@ class EventController extends Controller
     public function create(): View
     {
         $countries = Country::all();
-
-        return view('events.create',compact('countries'));
+        $tags = Tag::all();
+        return view('events.create',compact('countries','tags'));
     }
 
     /**
@@ -42,7 +44,8 @@ class EventController extends Controller
             $data['user_id'] =auth()->id();
             $data['slug'] = Str::slug($request->title);
 
-            Event::create($data);
+            $event = Event::create($data);
+            $event->tags()->attach($request->tags);
             return to_route('events.index');
         }else{
             return back();
